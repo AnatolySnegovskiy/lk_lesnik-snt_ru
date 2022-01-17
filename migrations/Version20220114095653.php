1a3fe7cde0c5abdcf2ababca14872a95754c23e0
Anatolii Snegovskii <CarrionGrow@Gmail.com>
2022-01-17 13:41:13 +0300

    fix migration

--- migrations/Version20220114095653.php
+++ migrations/Version20220114095653.php
@@ -20,7 +20,7 @@ final class Version20220114095653 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, personal_account VARCHAR(180) NOT NULL, roles CLOB NOT NULL --(DC2Type:json)
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, personal_account VARCHAR(180) NOT NULL, roles VARCHAR(255) NOT NULL COMMENT \'(DC2Type:json)\'
         , password VARCHAR(255) NOT NULL, is_verified BOOLEAN NOT NULL)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649FE115F8F ON user (personal_account)');
         $this->addSql('CREATE TABLE user_info (id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, user_id INTEGER DEFAULT NULL, name VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL)');
